a8ad37ca9472286931bbd0e596678e5817d75922
MaosyBaos <106411641+MaosyBaos@users.noreply.github.com>
2024-07-21 16:55:50 +0100

    LuaScans | Update Domain

--- src/LuaScans/LuaScans.ts
+++ src/LuaScans/LuaScans.ts
@@ -10,10 +10,10 @@ import {
     MangaStream
 } from '../MangaStream'
 
-const DOMAIN = 'https://luascans.com'
+const DOMAIN = 'https://luacomic.com'
 
 export const LuaScansInfo: SourceInfo = {
-    version: getExportVersion('0.0.0'),
+    version: getExportVersion('0.0.1'),
     name: 'LuaScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
@@ -32,4 +32,4 @@ export class LuaScans extends MangaStream {
     override configureSections() {
         this.homescreen_sections['new_titles'].enabled = false
     }
-}
+} 
